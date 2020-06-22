04b54da447ab6c3df21c5231388e4f9c1b51b1b4
thiagofis <Th516532>
2020-06-22 22:54:21 +0100

    WIP: Set up application module

--- application-publisher/src/main/java/com/marcolino/thiago/service/KafkaPublisherService.java
+++ application-publisher/src/main/java/com/marcolino/thiago/service/KafkaPublisherService.java
@@ -1,21 +1,33 @@
 package com.marcolino.thiago.service;
 
+import com.marcolino.thiago.KafkaMessage;
 import com.marcolino.thiago.Publisher;
 import com.marcolino.thiago.message.PublishingMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
+@org.springframework.stereotype.Service
 public class KafkaPublisherService implements PublisherService {
 
     private final Publisher publisher;
 
+    @Autowired
     public KafkaPublisherService(Publisher publisher) {
         this.publisher = publisher;
     }
 
     @Override
     public Future<Void> publish(PublishingMessage ... messages) {
-        Arrays.stream(messages).map(msg -> new KafkaMessage)
+        Runnable task = () -> {
+            Arrays.stream(messages)
+                  .map(msg -> new KafkaMessage(msg.getKey(), msg.getContent()))
+                  .forEach(msg -> publisher.publish(msg));
+        };
+
+        return CompletableFuture.runAsync(task);
     }
 }
